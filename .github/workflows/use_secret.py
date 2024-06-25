c80c5e849022aef4ebbe2647e3b583a8571a1f0b
daniilselin <dan.selin2004@mail.ru>
2024-06-25 12:44:35 +0700

    Нормализовал вывод для логов

--- .github/workflows/use_secret.py
+++ .github/workflows/use_secret.py
@@ -1,5 +1,5 @@
 import os, json
 token_data = os.environ['TOKEN_GOOGLE_API']
 token_path=os.path.join(os.path.dirname(__file__), "../../token.json")
-with open(token_data, 'w') as f:
+with open(token_path, 'w') as f:
     json.dump(json.loads(token_data), f, indent=4)
