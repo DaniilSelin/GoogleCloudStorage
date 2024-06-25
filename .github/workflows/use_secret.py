7bdb996781195603cde1ebe2f72c694c6b49c273
Пельмешка <113601705+DaniilSelin@users.noreply.github.com>
2024-06-25 14:37:44 +0700

    Update use_secret.py

--- .github/workflows/use_secret.py
+++ .github/workflows/use_secret.py
@@ -1,4 +1,5 @@
 import os, json
+"""Так как git secret ломает .json, мы его перезаписываем"""
 token_data = os.environ['TOKEN_GOOGLE_API']
 token_path=os.path.join(os.path.dirname(__file__), "../../token.json")
 with open(token_path, 'w') as f:
