9504085abd49e4a75040aa3fb10885841adc5b42
daniilselin <dan.selin2004@mail.ru>
2024-06-30 20:14:36 +0700

    ОБНОВЛЕНИЕ СТРУКТУРЫ ПРОЕКТА

--- .github/workflows/use_secret.py
+++ .github/workflows/use_secret.py
@@ -1,6 +1,6 @@
 import os, json
 """Так как git secret ломает .json, мы его перезаписываем"""
 token_data = os.environ['TOKEN_GOOGLE_API']
-token_path = os.path.join(os.path.dirname(__file__), "../../token.json")
+token_path = os.path.join(os.path.dirname(__file__), "../../encryption/token.json")
 with open(token_path, 'w') as f:
     json.dump(json.loads(token_data), f, indent=4)
